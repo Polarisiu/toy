2cd67cdc710b8f010d053a9916888e48960612be
Polaris <fdyrwaas@outlook.com>
2025-08-28 00:27:20 +0800

    Update nat.sh

--- nat.sh
+++ nat.sh
@@ -7,16 +7,17 @@ RESET="\033[0m"
 menu() {
     clear
     echo -e "${GREEN}=== 开小鸡管理菜单 ===${RESET}"
-    echo -e "${GREEN}1) KVM 小鸡${RESET}"
+    echo -e "${GREEN}1) PVE管理${RESET}"
     echo -e "${GREEN}2) LXC 小鸡${RESET}"
     echo -e "${GREEN}3) Docker 小鸡${RESET}"
+    echo -e "${GREEN}4) Incus 小鸡${RESET}"
     echo -e "${GREEN}0) 退出${RESET}"
     echo
     read -p $'\033[32m请选择操作 (0-3): \033[0m' choice
     case $choice in
         1)
-            echo -e "${GREEN}正在运行 KVM 小鸡脚本...${RESET}"
-            bash <(curl -fsSL https://raw.githubusercontent.com/Polarisiu/toy/main/kvm.sh)
+            echo -e "${GREEN}正在运行 PVE管理...${RESET}"
+            bash <(curl -sL https://raw.githubusercontent.com/Polarisiu/toy/main/pvegl.sh)
             pause
             ;;
         2)
@@ -27,6 +28,11 @@ menu() {
         3)
             echo -e "${GREEN}正在运行 Docker 小鸡脚本...${RESET}"
             bash <(curl -fsSL https://raw.githubusercontent.com/Polarisiu/toy/main/dockerlxc.sh)
+            pause
+            ;;
+        4)
+            echo -e "${GREEN}正在运行 Incus 小鸡脚本...${RESET}"
+            bash <(curl -sL https://raw.githubusercontent.com/Polarisiu/toy/main/incus.sh)
             pause
             ;;
         0)
