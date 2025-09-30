a6bb7090494f909bab5b7ec12f329db177ca9962
Polaris <fdyrwaas@outlook.com>
2025-09-30 13:51:51 +0800

    Update vpsw.sh

--- vpsw.sh
+++ vpsw.sh
@@ -6,7 +6,7 @@
 # ================== 配置 ==================
 SCRIPT_URL="https://raw.githubusercontent.com/Polarisiu/tool/main/network.sh"  # 脚本下载地址
 SCRIPT_PATH="/opt/vpsw/vps_network.sh"  # 脚本存放路径
-CONFIG_FILE="/opt/vpsw/.vps_tg_config"
+CONFIG_FILE="/opt/vpsw/.vps_tgg_config"
 OUTPUT_FILE="/tmp/vps_network_info.txt"
 
 GREEN='\033[0;32m'
