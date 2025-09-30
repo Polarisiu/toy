88b775fe37e2c9ddc657f2183ae6d01e0dd6faf3
Polaris <fdyrwaas@outlook.com>
2025-09-30 11:00:20 +0800

    Update vpsx.sh

--- vpsx.sh
+++ vpsx.sh
@@ -293,7 +293,7 @@ pause_return(){
 
 # ================== 卸载脚本 ==================
 uninstall_script(){
-    echo -e "${yellow}⚠️ 即将卸载 tg.sh 脚本及配置和定时任务${re}"
+    echo -e "${yellow}⚠️ 即将卸载脚本及配置和定时任务${re}"
     read -rp "确认卸载吗？(y/N): " confirm
     if [[ "$confirm" =~ ^[Yy]$ ]]; then
         CRON_CMD="bash $SCRIPT_PATH send"
