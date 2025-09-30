8e40f5c50911be99b6b6de96e0efddc2af75fcf8
Polaris <fdyrwaas@outlook.com>
2025-09-30 08:03:39 +0800

    Update pvegl.sh

--- pvegl.sh
+++ pvegl.sh
@@ -16,7 +16,7 @@ menu() {
     echo -e "${GREEN}7) 开设 LXC 小鸡${RESET}"
     echo -e "${GREEN}0) 退出${RESET}"
     echo
-    read -p $'\033[32m请选择操作 (0-7): \033[0m' choice
+    read -p $'\033[32m请选择操作: \033[0m' choice
     case $choice in
         1)
             echo -e "${GREEN}正在添加 SWAP...${RESET}"
